c557849ead0c62fe78118b4a64c1fe5b3aba5bda
CjMonteclaro <cjhmonteclaro@gmail.com>
2022-09-14 17:15:51 +0800

    Added data fetching for discover page.

--- pages/discover.tsx
+++ pages/discover.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
+import api from "../pages/api/api"
 import styles from '../styles/Booklist.module.css'
 
-const discover = () => (
+const Discover = () => {
+  const [bookData, setBookData] = useState([]) 
+
+  useEffect(() => {
+    const token = localStorage.getItem("token")
+    api.get("books", {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': token
+      } 
+    })
+    .then(function (response) {
+      setBookData(response.data.data)
+      return response
+    })
+    .then(function (data) {
+      return data
+    })
+    .catch(function (error: Error) {
+      console.log(error)
+    })
+  }, [])
+
+  return(
   <>
     <Head>
       <title>Discover New Books</title>
@@ -13,31 +37,32 @@ const discover = () => (
     <main>
       <p>Welcome to the discover page.</p>
     </main>
+    
+    {bookData && bookData.map((book) => ( 
+      <div key={book.attributes.id}>
+        <div className={styles.card}>
+          <div className={styles.grid}>
+            <div>
+              <Image src={book.attributes.cover_image_url} alt={book.attributes.title} width={150} height={250} />
+            </div>
+            <div>
+              <h2>{book.attributes.title}</h2>
+              <p>{book.attributes.author}</p>
+              <p>{book.attributes.publisher}</p>
 
-    <div>
-      <div className={styles.card}>
-        <div className={styles.grid}>
-          <div>
-            <Image src="/padington.jpg" alt="Padington" width={150} height={250} />
-          </div>
-          <div>
-            <h2>A Bear Called Paddington</h2>
-            <p>Michael Bond | HarperCollins</p>
+              <small>{book.attributes.synopsis}</small>
 
-            <small>
-              Paddington Bear had just traveled all the way from Peru when the Brown family first met him in Paddington Station. Since then, their lives have never been quite the same . . . for ordinary things become extraordinary when Paddington is involved.
-            </small>
-
-            <div className={styles.grid}>
-              <button className={styles.button}>
-                <Link href="#">Add to list</Link>
-              </button>
+              <div className={styles.grid}>
+                <button className={styles.button}>
+                  <Link href="#">Add to list</Link>
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    ))}
   </>
-)
+)}
 
-export default discover
+export default Discover
